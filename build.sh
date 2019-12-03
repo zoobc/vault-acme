0a0351d4241ac59745ed61aa49dcdb43b6d972e4
heri16 <527101+heri16@users.noreply.github.com>
2019-12-04 00:07:06 +0800

    Add zip step to build.sh

--- build.sh
+++ build.sh
@@ -4,6 +4,11 @@ TAG=remilapeyre/vault-acme:build
 echo Building $TAG
 docker build -t "$TAG" . -f Dockerfile.build
 
+echo Copying builds from $TAG
 docker container create --name extract "$TAG" 
 docker container cp extract:/go/src/github.com/remilapeyre/vault-acme/build ./build
 docker container rm -f extract
+
+echo Zipping builds from $TAG
+find ./build -type d -maxdepth 1 -mindepth 1 -exec basename {} \; | xargs -I{} zip -j -r ./build/acme-plugin_{}.zip ./build/{} -i "*acme-plugin*"
+find ./build -type d -maxdepth 1 -mindepth 1 -exec basename {} \; | xargs -I{} zip -j -r ./build/sidecar_{}.zip ./build/{} -i "*sidecar*"
